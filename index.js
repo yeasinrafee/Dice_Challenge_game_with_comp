1a0461c24efe43e7a41e30228406c9c3514f3857
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-07-25 00:39:03 +0600

    Final Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// for Player 1 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;   // pick number 1-6
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";   // pick images 1-6
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+// for Player 2 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// if player 1 won
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 won!!";
+}
+// if player 2 won
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 won!!";
+} 
+// if draw
+else {
+    document.querySelector("h1").innerHTML = "Match Draw :)";
+}
